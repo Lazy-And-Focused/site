d1f21f0d3caaf39574fe9760aee5d10f8f31d2d6
lanvalird <strazhognay@gmail.com>
2025-07-11 21:25:09 +0400

    -fix `prettier-config-laf` in project

---
+++ .prettier.config.mjs
@@ -0,0 +1,8 @@
+import { PrettierLafConfiguration } from "prettier-config-laf";
+
+/** @type {import("prettier").Config} */
+const config = {
+  ...PrettierLafConfiguration,
+};
+
+export default config;
