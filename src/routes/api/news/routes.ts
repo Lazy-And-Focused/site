583680bfdc15064951551dea8c23a98d3cdd998c
lanvalird <lanvalird@strazhognay@gmail.com>
2025-07-16 14:00:22 +0000

    Publishing changes to backend

---
+++ src/routes/api/news/routes.ts
@@ -0,0 +1,12 @@
+const ROUTES = {
+  GET_ALL: "/",
+  GET_ONE: "/:name",
+} as const;
+
+const QUERY = {
+  GET: ["length", "offset"] as const,
+} as const;
+
+const ROUTER = "/news/" as const;
+
+export { ROUTER, ROUTES, QUERY };
