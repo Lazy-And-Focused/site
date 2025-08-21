88a2f9aa494fd5bc2999a13d91259bb951405a4c
lanvalird <strazhognay@gmail.com>
2025-08-21 16:54:44 +0400

    -up header: add backdrop-blur in/on header

--- apps/frontend/www/widgets/header/index.tsx
+++ apps/frontend/www/widgets/header/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 border-b-2 border-b-primary/70 border-dashed">
+    <header className="fixed top-0 right-0 left-0 z-50 border-b-2 border-b-primary/70 border-dashed backdrop-blur-md">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-10">
         <div className="flex h-16 items-center justify-between lg:h-20">
           <Link href="/" className="flex items-center space-x-2">
