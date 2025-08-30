2cabbc8aad76577cba19bbd2bf788069f292090a
lanvalird <strazhognay@gmail.com>
2025-08-30 17:08:16 +0400

    -ref-format clean up + prettier format use

--- apps/frontend/www/widgets/header/index.tsx
+++ apps/frontend/www/widgets/header/index.tsx
@@ -6,57 +6,36 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@radix-ui/react-navigation-menu";
-import { HomeIcon, LucideProps } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { ForwardRefExoticComponent, RefAttributes } from "react";
 
-type NavigationItem = {
-  name: string;
-  icon?: ForwardRefExoticComponent<
-    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
-  >;
-  description?: string;
-
-  href?: string;
-
-  children?: React.ReactNode;
-};
-
-const NAVIGATION_LINKS: NavigationItem[] = [
-  {
-    name: "Главная",
-    href: "/",
-  },
-];
+import { NAVIGATION_LINKS as links } from "@/shared/constants/navigation-links";
 
 export function Header() {
-  const links = NAVIGATION_LINKS;
-
   return (
-    <header className='fixed top-0 right-0 left-0 z-50 border-b-2 border-b-primary/70 border-dashed backdrop-blur-md'>
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-10'>
-        <div className='flex h-16 items-center justify-between lg:h-20'>
-          <Link href='/' className='flex items-center space-x-2'>
-            <div className='relative flex h-9 w-9 items-center justify-center rounded-sm bg-primary overflow-hidden'>
+    <header className="fixed top-0 right-0 left-0 z-50 border-b-2 border-b-primary/70 border-dashed backdrop-blur-md">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-10">
+        <div className="flex h-16 items-center justify-between lg:h-20">
+          <Link href="/" className="flex items-center space-x-2">
+            <div className="relative flex h-9 w-9 items-center justify-center rounded-sm bg-primary overflow-hidden">
               <Image
-                alt='Logo'
-                className='h-full w-full'
-                src='/logo.png'
+                alt="Logo"
+                className="h-full w-full"
+                src="/logo.png"
                 width={64}
                 height={64}
               />
             </div>
           </Link>
 
-          <NavigationMenu className='text-sm space-x-8'>
+          <NavigationMenu className="text-sm space-x-8">
             <NavigationMenuList>
               {links.map(({ href, name, icon: Icon }) => (
                 <NavigationMenuItem key={name}>
                   <NavigationMenuLink asChild>
                     <Link
                       href={href || ""}
-                      className='flex flex-row items-center gap-2 text-primary leading-none hover:text-primary/80 hover:underline'
+                      className="flex flex-row items-center gap-2 text-primary leading-none hover:text-primary/80 hover:underline"
                     >
                       {Icon ? <Icon width={16} height={16} /> : null} {name}
                     </Link>
