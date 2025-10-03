80439b25dc37936e39a148e9aaf946e3c79eaf02
FOCKUSTY <wwddwwwd@mail.ru>
2025-10-03 21:08:55 +0500

    -add submit butto

--- apps/dashboard/src/app/editor/page.tsx
+++ apps/dashboard/src/app/editor/page.tsx
@@ -1,12 +1,39 @@
 'use client'
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { NEWS_API_OBJECT, NEWS_API_VARIANTS } from "../api/constants";
 
 import styles from "./page.module.css";
 
 const Page = () => {
   const [ choosedApi, setChoosedApi ] = useState<string>(NEWS_API_VARIANTS[0]);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  const chooseApi = (api: string) => {
+    setChoosedApi(api);
+
+    if (!textRef.current) {
+      return;
+    };
+
+    const exceedsMaxLength = textRef.current.value.length > NEWS_API_OBJECT[choosedApi].maxTextLength
+    if (!exceedsMaxLength) {
+      return;
+    };
+
+    alert("Вы переключились на другую соцсеть, у вас превышен лимит букв");
+  };
+
+  const post = () => {
+    if (!textRef.current) {
+      return;
+    }
+
+    const api = choosedApi;
+    const text = textRef.current.value;
+
+    console.log({api, text});
+  }
 
   return (
     <div className="page-center">
@@ -26,7 +53,7 @@ const Page = () => {
                   .map(api =>
                     <button
                       key={api}
-                      onClick={() => setChoosedApi(api)}
+                      onClick={() => chooseApi(api)}
                     >
                       {api}
                     </button>
@@ -42,10 +69,13 @@ const Page = () => {
           <textarea
             name="text"
             id="text"
+            ref={textRef}
             maxLength={NEWS_API_OBJECT[choosedApi].maxTextLength}
             placeholder={`Напишите сюда свой текст для ${choosedApi}`}
           ></textarea>
         </section>
+
+        <button onClick={post}>Submit</button>
       </div>
     </div>
   )
