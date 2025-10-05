8f5c6eb4f4994f01bf262ffcaff58e39b29f6729
FOCKUSTY <wwddwwwd@mail.ru>
2025-10-05 10:29:29 +0500

    -up api.ts, bluesky.api: Promise<void> -> Promise<unknown>

--- apps/backend/src/api/api.ts
+++ apps/backend/src/api/api.ts
@@ -20,15 +20,15 @@ export type Parameters = {
 };
 
 export type Methods = {
-  [P in (typeof METHODS)[number]]: (data: Parameters[P]) => Promise<void>;
+  [P in (typeof METHODS)[number]]: (data: Parameters[P]) => Promise<unknown>;
 };
 
 export abstract class Api implements Methods {
   public abstract readonly maxTextLength: number;
 
-  public abstract post(data: { text: string }): Promise<void>;
-  public abstract put(data: { id: string, text: string }): Promise<void>;
-  public abstract delete(data: {id: string}): Promise<void>;
+  public abstract post(data: { text: string }): Promise<unknown>;
+  public abstract put(data: { id: string, text: string }): Promise<unknown>;
+  public abstract delete(data: {id: string}): Promise<unknown>;
 }
 
 export default Api;
