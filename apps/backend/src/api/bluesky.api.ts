bcdb93d9932db9fbcd23fe022e0c7f4f52a052e4
FOCKUSTY <wwddwwwd@mail.ru>
2025-10-05 10:29:39 +0500

    -aboboa

--- apps/backend/src/api/bluesky.api.ts
+++ apps/backend/src/api/bluesky.api.ts
@@ -3,15 +3,15 @@ import Api from "./api";
 export class BlueskyApi extends Api {
   public readonly maxTextLength: number = 300;
 
-  public post({ text }: { text: string }): Promise<void> {
+  public post({ text }: { text: string }): Promise<unknown> {
     throw new Error(JSON.stringify({text}));
   }
 
-  public put({ id, text }: { id: string, text: string }): Promise<void> {
+  public put({ id, text }: { id: string, text: string }): Promise<unknown> {
     throw new Error(JSON.stringify({id, text}));
   }
   
-  public delete({ id }: { id: string }): Promise<void> {
+  public delete({ id }: { id: string }): Promise<unknown> {
     throw new Error(JSON.stringify({id}));
   }
 }
