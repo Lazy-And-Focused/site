d034c461d2158640edfd9ea932f82f15718e028b
lanvalird <strazhognay@gmail.com>
2025-10-06 12:48:11 +0400

    -up loading page

---
+++ apps/frontend/src/pages/loading/index.tsx
@@ -0,0 +1,18 @@
+import { Loading } from '@shared/components/loading';
+
+const LoadingPage = () => {
+  return (
+    <main
+      className={[
+        'h-screen max-h-screen w-full',
+        'bg-cover bg-center bg-no-repeat',
+        "bg-[#d88188] bg-[url('/images/backgrounds/pink.png')]",
+        'dark:bg-[#205848] dark:bg-none',
+      ].join(' ')}
+    >
+      <Loading />
+    </main>
+  );
+};
+
+export default LoadingPage;
