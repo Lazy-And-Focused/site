17eda80c32e911d1def156980ddb39f900a79812
lanvalird <strazhognay@gmail.com>
2025-10-10 19:21:38 +0400

    -fix yes

--- apps/frontend/src/pages/projects/index.tsx
+++ apps/frontend/src/pages/projects/index.tsx
@@ -5,14 +5,16 @@ import { useProjects } from './hooks/use-projects';
 
 import { PROJECTS_PAGE_CONSTANTS } from './lib/constants';
 
+const placeholder = PROJECTS_PAGE_CONSTANTS.BROWSER.EMPTY_TEXT;
+const loadingPlaceholder = <div className='mt-4 text-slate-200'>Загрузка проектов...</div>;
+
 const ProjectsPage = () => {
   const { tabs, loaded } = useProjects();
 
   return (
     <main className='h-full w-full'>
       <ProjectsHero>
-        <Browser tabs={tabs}>{PROJECTS_PAGE_CONSTANTS.BROWSER.EMPTY_TEXT}</Browser>
-        {!loaded && <div className='mt-4 text-slate-200'>Загрузка проектов...</div>}
+        <Browser tabs={tabs}>{!loaded ? loadingPlaceholder : placeholder}</Browser>
       </ProjectsHero>
     </main>
   );
