114334ce3975d682404aa99ef4604e3c16dc999b
lanvalird <strazhognay@gmail.com>
2025-12-10 14:12:10 +0400

    -up modals (merged from header & member-card)

---
+++ apps/frontend/src/shared/components/app-header/ui/navigation/modal.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from 'react';
+
+import { BaseModal, BaseModalProps } from '@shared/components/base-modal';
+
+export const AppHeaderModalNavigation = forwardRef<HTMLDialogElement, BaseModalProps>(
+  (props, ref) => {
+    return (
+      <nav className='flex justify-center gap-x-4 sm:gap-x-8 lg:gap-x-12'>
+        <BaseModal ref={ref} {...props} closeButtonPosition='top' showDivider={false} />
+      </nav>
+    );
+  },
+);
