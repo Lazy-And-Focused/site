5a3371dcb449ce78b3be009b2a7c15790a43387a
lanvalird <strazhognay@gmail.com>
2026-02-08 19:57:04 +0400

    -up export styles app-footer to `STYLE` const

--- apps/frontend/src/shared/components/app-footer/index.tsx
+++ apps/frontend/src/shared/components/app-footer/index.tsx
@@ -1,14 +1,22 @@
 import { Link } from './link';
-
 import { APP_ROUTES } from '@shared/lib/constants';
+
+const STYLE = {
+  CONTAINER: [
+    'w-full border-t-2 border-primary/75',
+    'bg-neutral/20 p-4 backdrop-blur-md',
+    'transition duration-500 ease-in-out',
+  ].join(' '),
+  NAV: [
+    'container mx-auto flex flex-wrap items-center justify-center',
+    'px-4 py-2 text-primary sm:justify-start lg:px-8',
+  ].join(' '),
+} as const;
 
 export const AppFooter = () => {
   return (
-    <footer className='w-full border-t-2 border-primary/75 bg-neutral/20 p-4 backdrop-blur-md transition duration-500 ease-in-out'>
-      <nav
-        className='container mx-auto flex flex-wrap items-center justify-center px-4 py-2 text-primary sm:justify-start lg:px-8'
-        aria-label='Global'
-      >
+    <footer className={STYLE.CONTAINER}>
+      <nav className={STYLE.NAV} aria-label='Global'>
         <Link href={APP_ROUTES.SOCIALS}>Ссылки на команду</Link>
         <Link href={APP_ROUTES.COPYRIGHT}>Авторские права</Link>
       </nav>
