9abf59c201da458c3549184e404e2899318895c6
lanvalird <strazhognay@gmail.com>
2026-03-09 16:52:44 +0400

    chore(frontend): format files

--- apps/frontend/src/pages/home/style.ts
+++ apps/frontend/src/pages/home/style.ts
@@ -1,5 +1,5 @@
 export const STYLE = {
   CONTAINER: 'h-full w-full',
   PRIMARY_LINK: 'btn btn-primary rounded-full',
-  SECONDARY_LINK: 'btn btn-ghost rounded-full text-slate-200'
+  SECONDARY_LINK: 'btn btn-ghost rounded-full text-slate-200',
 } as const;
