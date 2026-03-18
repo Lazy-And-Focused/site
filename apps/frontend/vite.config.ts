1b76657e2451f6d105406adc272746c51290273d
lanvalird <strazhognay@gmail.com>
2026-03-18 15:36:35 +0400

    fix(frontend/vercel): fix import into vite config file

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vercel from 'vite-plugin-vercel/vite';
+import { vercel } from 'vite-plugin-vercel/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
